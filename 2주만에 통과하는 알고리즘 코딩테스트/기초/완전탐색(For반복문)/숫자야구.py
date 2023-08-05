0b791b72506c4fcf06ed998f31e2369e40d77377
jade <jadejung95@gmail.com>
2023-08-05 16:41:35 +0900

    완전탐색(For반복문) : Brute Force

--- 2주만에 통과하는 알고리즘 코딩테스트/기초/완전탐색(For반복문)/숫자야구.py
+++ 2주만에 통과하는 알고리즘 코딩테스트/기초/완전탐색(For반복문)/숫자야구.py
@@ -1,45 +1,59 @@
-# A가 정답으로 생각할 수 있는 모든 수를 넣어보기!
+n = int(input())
 
-# 그리고 B가 도전한 내용에 맞는지 확인하기!
-
-n = int(input()) # 4
-
-hint = [list(map(int, input().split())) for _ in range(4)]
-
-# [[123,1,1],[356,1,0], ...]
+numbers = [list(map(str,input().split())) for _ in range(n)]
+# string을 미리 넣어준 이유는, 나중에 쪼개기 위해서
 
 answer = 0
-# 100 ~ 999
-for a in range(1,10): #100의 자리수
-    for b in range(10): #10의 자리수
-        for c in range(10): #1의 자리수
 
-            if ( a == b and b == c and c == a ): 
-                continue # 무시 
+# (1) 세 자리 숫자 만들기
+for a in range(1,10): # 100의 자리수
+    for b in range(1,10): # 10의 자리수
+        for c in range(1,10): # 1의 자리수
+            counter = 0
             
-            # 다른 숫자로 이루어진 세 자리수
+            # (2) 다른 세 자리수
+            if( a == b or b == c or c == a):
+                continue
+            
+            # continue, 그 숫자를 넘김
+            # break, 반복문을 넘김
 
-            # 숫자가 정해졌다면
-            cnt = 0
-            for arr in hint:
-                number = hint[0]
-                ball = hint[1]
-                strike = hint[2]
+            # (3) 배열에 넣은 조건을 넣어주기
+            for array in numbers:
+                check = list(array[0]) # ['1','2','3']
+                strike = int(array[1])
+                ball = int(array[2])
 
-                # a, b, c 라는 숫자
-                # number 하고 비교해서
-
-                # 자리수를 나눠서, strike ball 을 측정하는 부분
-
-                ball_count = 0
                 strike_count = 0
+                ball_count = 0
 
-                if ball == ball_count and strike == strike_count:
-                    cnt += 1
-    
-            if cnt == n:
+                #스트라이크 계산기
+                if (a == int(check[0])):
+                    strike_count += 1
+                if (b == int(check[1])):
+                    strike_count += 1
+                if (c == int(check[2])):
+                    strike_count += 1
+                
+
+                #볼 계산기
+                if (a == int(check[1]) or a == int(check[2])):
+                    ball_count += 1
+                if (b == int(check[0]) or b == int(check[2])):
+                    ball_count += 1
+                if (c == int(check[0]) or c == int(check[1])):
+                    ball_count += 1
+                
+                
+                #(4) 매칭 여부 확인하기
+                if (strike != strike_count):
+                    break
+                if (ball != ball_count):
+                    break
+                
+                counter += 1
+
+            if counter == n:
                 answer += 1
-
+                
 print(answer)
-
-
